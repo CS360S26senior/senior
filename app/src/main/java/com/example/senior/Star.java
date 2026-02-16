03192287794ea7ff0743419e054a372ba5ce92b2
zahratirmizey <26100147@lums.edu.pk>
2026-02-17 01:53:09 +0500

    Add Star model extending Shape

---
+++ app/src/main/java/com/example/senior/Star.java
@@ -0,0 +1,20 @@
+package com.example.senior;
+
+public class Star extends Shape {
+
+	private int points;
+
+	public Star(int x, int y. int points) {
+		super(x,y)
+		this.points = points;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+}
